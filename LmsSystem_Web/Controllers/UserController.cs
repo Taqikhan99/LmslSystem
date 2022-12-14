db626b3e2ca0bb71eea94aaf053c6d741d4b69aa
TaqiAhmed <taqiahmed339@gmail.com>
2022-12-14 17:25:05 +0500

    Made Admin HomePage, Display students Page Create New User Page

--- LmsSystem_Web/Controllers/UserController.cs
+++ LmsSystem_Web/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LmsSystem_DAL.Abstract;
+using LmsSystem_DAL.Concrete;
+using LmsSystem_DAL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,8 +14,12 @@ namespace LmsSystem_Web.Controllers
     public class UserController : Controller
     {
         // GET: User
+        private IUserRepository _userRepo;
 
-        
+        public UserController()
+        {
+            this._userRepo = new UserRepository();
+        }
         public ActionResult Index()
         {
             return View();
@@ -27,6 +34,16 @@ namespace LmsSystem_Web.Controllers
         {
             return View();
         }
+
+        //method to get student list
+        [Authorize(Roles = "Admin")]
+        public ActionResult GetStudents() {
+
+            List<User> users = _userRepo.GetStudents();
+
+            return View(users);
+        }
+
 
         
 
